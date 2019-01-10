0b0ed9d680c20d602f63d61e1734926d5072eabc
Carol - Macbook <carolfsampaio@gmail.com>
2019-01-09 23:49:34 -0300

    fixed constraints and increased font size for labelResult. created all outlets. created function for clear labelResult and detect witch number was clicked.

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -10,11 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var allClear: UIButton!
+    @IBOutlet weak var percentage: UIButton!
+    @IBOutlet weak var divide: UIButton!
+    @IBOutlet weak var multiply: UIButton!
+    @IBOutlet weak var subtract: UIButton!
+    @IBOutlet weak var sum: UIButton!
+    @IBOutlet weak var equals: UIButton!
+    @IBOutlet weak var dot: UIButton!
+    @IBOutlet weak var zero: UIButton!
+    @IBOutlet weak var one: UIButton!
+    @IBOutlet weak var two: UIButton!
+    @IBOutlet weak var three: UIButton!
+    @IBOutlet weak var four: UIButton!
+    @IBOutlet weak var five: UIButton!
+    @IBOutlet weak var six: UIButton!
+    @IBOutlet weak var seven: UIButton!
+    @IBOutlet weak var eight: UIButton!
+    @IBOutlet weak var nine: UIButton!
+    @IBOutlet weak var labelResult: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        allClear(self)
     }
 
-
+    @IBAction func allClear(_ sender: Any) {
+        labelResult.text = "0"
+    }
+    
+    @IBAction func numberClicked(_ sender: UIButton) {
+        if labelResult.text == "0" {
+            labelResult.text = sender.titleLabel?.text
+        } else {
+            if let numberLabel = sender.titleLabel?.text {
+                labelResult.text?.append(numberLabel)
+            }
+        }
+    }
+    
+    
 }
 
