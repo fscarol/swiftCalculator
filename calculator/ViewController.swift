ae9981e2e1bddd65aeab779bc1e1d73998d26b07
Carol - Macbook <carolfsampaio@gmail.com>
2019-02-03 15:47:24 -0300

    created all functions needed to a calculator work properly.

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -10,8 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var numberWasClicked: Bool = false
+    var a: Double = 0
+    var b: Double?
+    enum OperationNames: String {
+        case sum
+        case subtract
+        case multiply
+        case divide
+        case equals
+        case percent
+    }
+    var lastOperation: OperationNames?
+    
     @IBOutlet weak var allClear: UIButton!
-    @IBOutlet weak var percentage: UIButton!
+    @IBOutlet weak var percent: UIButton!
     @IBOutlet weak var divide: UIButton!
     @IBOutlet weak var multiply: UIButton!
     @IBOutlet weak var subtract: UIButton!
@@ -39,18 +52,97 @@ class ViewController: UIViewController {
 
     @IBAction func allClear(_ sender: Any) {
         labelResult.text = "0"
+        a = 0
+        b = nil
+        numberWasClicked = false
     }
     
     @IBAction func numberClicked(_ sender: UIButton) {
-        if labelResult.text == "0" {
+        
+        if labelResult.text == "0" || !numberWasClicked {
             labelResult.text = sender.titleLabel?.text
+            if lastOperation == .equals {
+                a = 0
+            }
         } else {
             if let numberLabel = sender.titleLabel?.text {
                 labelResult.text?.append(numberLabel)
             }
         }
+        numberWasClicked = true
+        
     }
     
+    @IBAction func operation(_ sender: UIButton) {
+        if numberWasClicked {
+            numberWasClicked = false
+            if a == 0 {
+                if let temp: String = labelResult.text {
+                    a = Double(temp)!
+                } else {
+                    return
+                }
+            } else {
+                if b == nil {
+                    if let temp: String = labelResult.text {
+                        b = Double(temp)!
+                    } else {
+                        return
+                    }
+                    switch lastOperation {
+                    case .sum? :
+                        a = a + b!
+                    case .subtract?:
+                        a = a - b!
+                    case .multiply?:
+                        a = a * b!
+                    case .divide?:
+                        if b != 0 {
+                            a = a / b!
+                        } else {
+                            b = nil
+                            labelResult.text = "Not a number"
+                            return
+                        }
+                    default:
+                        print("There is no operation")
+                    }
+                    labelResult.text = String(a)
+                    b = nil
+                }
+            }
+        }
+        switch sender {
+        case sum:
+            lastOperation = .sum
+        case subtract:
+            lastOperation = .subtract
+        case multiply:
+            lastOperation = .multiply
+        case divide:
+            lastOperation = .divide
+        case equals:
+            lastOperation = .equals
+        case percent:
+            lastOperation = .percent
+            a = a / 100
+            labelResult.text = String(a)
+        default:
+            lastOperation = .none
+        }
+    }
     
+    @IBAction func dotClicked() {
+        if !numberWasClicked {
+            labelResult.text = "0"
+        }
+        let displayText = labelResult?.text
+        if displayText!.contains(".") {
+            return
+        } else {
+            labelResult.text?.append(".")
+            numberWasClicked = true
+        }
+    }
 }
 
